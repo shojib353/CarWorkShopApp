e420f325492951ba91c7cb19857fd7249661c956
Md. Ruman <ruman.cse49@gmail.com>
2025-01-13 15:56:52 +0600

    Changes BottomNavBar and some UI colors

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -2,12 +2,17 @@ import 'package:car_workshop_mobile_app/model/booking_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen(
-      {super.key, required this.onAddMechanicTap, required this.role});
+import 'calender_screen.dart';
 
-  final void Function() onAddMechanicTap;
-  final String role;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+    required this.userName,
+    required this.userRole,
+  });
+
+  final String userRole;
+  final String userName;
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -35,7 +40,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
     setState(() {
       bookings = snapshot.docs.map((doc) => doc.data()).toList();
-      print(bookings);
     });
   }
 
@@ -43,80 +47,66 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Center(
-        child: bookings.isEmpty
-            ? const CircularProgressIndicator()
-            : (widget.role == 'Admin')
-                ? ListView.builder(
-                    itemCount: bookings.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Card(
-                          elevation: 3,
-                          child: Row(
-                            children: [
-                              const SizedBox(width: 10),
-                              CircleAvatar(
-                                radius: 25,
-                                child: Text(
-                                  '${index + 1}',
-                                  style: const TextStyle(fontSize: 22),
+          child: bookings.isEmpty
+              ? const CircularProgressIndicator()
+              : (widget.userRole == 'Admin')
+                  ? ListView.builder(
+                      itemCount: bookings.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Card(
+                            elevation: 3,
+                            child: Row(
+                              children: [
+                                const SizedBox(width: 10),
+                                CircleAvatar(
+                                  radius: 25,
+                                  child: Text(
+                                    '${index + 1}',
+                                    style: const TextStyle(fontSize: 22),
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(width: 20),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      bookings[index].title!,
-                                      style: const TextStyle(
-                                        color: Color.fromARGB(255, 25, 25, 25),
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
+                                const SizedBox(width: 20),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        bookings[index].title!,
+                                        style: const TextStyle(
+                                          color: Colors.teal,
+                                          // Color.fromARGB(255, 25, 25, 25),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Text(
-                                      'Assigned Mechanic: ${bookings[index].mechanic!}',
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Text(
-                                      'Start: ${bookings[index].start!}',
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Text('End: ${bookings[index].end!}'),
-                                    const SizedBox(height: 3),
-                                    Text('Plate: ${bookings[index].plate!}')
-                                  ],
-                                ),
-                              )
-                            ],
+                                      const SizedBox(height: 3),
+                                      Text(
+                                        'Assigned Mechanic: ${bookings[index].mechanic!}',
+                                      ),
+                                      const SizedBox(height: 3),
+                                      Text(
+                                        'Start: ${bookings[index].start!}',
+                                      ),
+                                      const SizedBox(height: 3),
+                                      Text('End: ${bookings[index].end!}'),
+                                      const SizedBox(height: 3),
+                                      Text('Plate: ${bookings[index].plate!}')
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    })
-                : Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    child: Container(
-                      height: 200,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.deepOrange.shade400),
-                      child: const Center(
-                        child: Text(
-                          'Welcome to Car Servicing App',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-      ),
+                        );
+                      })
+                  : CalenderScreen(
+                      userName: widget.userName,
+                      userRole: widget.userRole,
+                    )),
     );
   }
 }
